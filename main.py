adef73fe6abfb5f194d8f3a5705bf905fb06e998
degaus72 <30686993+degaus72@users.noreply.github.com>
2023-06-13 14:45:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 from Tone import Tone
 
 # This is the letter 'P'
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.15)   # da
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.15)   # da
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.15)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.72)   # da
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.72)   # da
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.72)     # intra-char
 
 # This is the letter 'A'
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.15)   # da
-Tone.sine(0, duration=0.15)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.72)   # da
+Tone.sine(0, duration=0.72)     # intra-char
 
 # This is the letter 'R'
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.15)   # da
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.15)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.72)   # da
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.72)     # intra-char
 
 # This is the letter 'I'
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.15)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.72)     # intra-char
 
 # This is the letter 'S'
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.05)     # intra-char
-Tone.sine(750, duration=0.05)   # di
-Tone.sine(0, duration=0.15)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.24)     # intra-char
+Tone.sine(750, duration=0.24)   # di
+Tone.sine(0, duration=0.72)     # intra-char
